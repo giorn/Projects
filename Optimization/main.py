bb3499c0f64ed71400780c3661522ac7789daf29
Gregoire Caron <g.caron1789@gmail.com>
2024-10-29 19:03:22 +0100

    Added class Failure

--- Optimization/main.py
+++ Optimization/main.py
@@ -1,17 +1,33 @@
 import numpy as np
 from scipy.optimize import minimize
-import matplotlib.pyplot as plt
 
 import Benchmark_Functions.pybenchfunction as bench
 
+
+class Failure():
+
+    def __init__(self, func, failure_proba):
+        self.func = func
+        self.failure_proba = failure_proba
+    
+    def function(self, x):
+        """Modified benchmark function with a probability of failure."""
+        unif_nb = np.random.uniform(0, 1)
+        if unif_nb <= self.failure_proba:
+            print("Crash")
+            return "Crash"
+        else:
+            print("here")
+            return self.func(x)
 
 # Set up benchmark function
 dim = 3
 func = bench.function.Rosenbrock(dim)
 X_min, minimum = func.get_global_minimum(dim)
-print(X_min, minimum)
 
 # Optimization
 x0 = [0.0, 2.0, 0.0]
-res = minimize(func, x0, method='BFGS')
+method = "BFGS"
+fail = Failure(func, 0.01)
+res = minimize(fail.function, x0, method=method)
 print(res.x)
