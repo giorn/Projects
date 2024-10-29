e62c11500277b0950c6f0b0dbea5ca4236a470ad
Gregoire Caron <g.caron1789@gmail.com>
2024-10-29 16:32:23 +0100

    Separated unit tests

--- Unit_tests/test_sensitivity.py
+++ Unit_tests/test_sensitivity.py
@@ -15,17 +15,21 @@ def experiment():
     scaler_path = "Sensitivity_analysis/regression_scaler.gz"
     model = tf.keras.models.load_model(model_path)
     scaler = joblib.load(scaler_path)
-    X1 = (np.random.rand(100)*2*np.pi).reshape(-1, 1)
-    X2 = (np.random.rand(100)).reshape(-1, 1)
+    X1 = (np.random.rand(1000)*2*np.pi).reshape(-1, 1)
+    X2 = (np.random.rand(1000)).reshape(-1, 1)
     data = np.concatenate((X1, X2), axis=1)
     return Sensitivity(model, scaler, data)
 
-def test_data_shape(experiment):
+def test_data_shape_00(experiment):
     """Test that the derivatives are the ones expected."""
     experiment.compute_gradient()
     mu, sigma, mu_sqd = experiment.get_measures(0, 0)
     assert mu == pytest.approx(0, abs=0.10)
     assert sigma == pytest.approx(np.sqrt(2)/2, abs=0.10)
+
+def test_data_shape_01(experiment):
+    """Test that the derivatives are the ones expected."""
+    experiment.compute_gradient()
     mu, sigma, mu_sqd = experiment.get_measures(0, 1)
     assert mu == pytest.approx(0, abs=0.10)
     assert sigma == pytest.approx(3*np.sqrt(2)/2, abs=0.10)
