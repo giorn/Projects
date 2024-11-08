a2a1175d8b51a81659825c8ecc5224a053638620
Gregoire Caron <g.caron1789@gmail.com>
2024-11-08 23:52:35 +0100

    Added adaptive proposal std

--- MCMC/main.py
+++ MCMC/main.py
@@ -3,6 +3,7 @@ Author: Gregoire Caron
 Date Created: 2024-11-08
 Last Modified: 2024-11-08
 Small example of application of the Markov Chain Monte-Carlo Metropolis-Hastings algorithm.
+Useful when sampling a distribution using basic Monte-Carlo is too long or expensive.
 """
 
 import numpy as np
@@ -11,7 +12,7 @@ import matplotlib.pyplot as plt
 
 def target_distribution(x):
     """Return the probability at x for a mixture of Gaussian distributions."""
-    means = [2, -2]
+    means = [4, -2]
     stds = [0.5, 0.8]
     weights = [0.3, 0.7]
     proba = np.sum([w*np.exp(-0.5*((x-m)/s)**2) for w, m, s in zip(weights, means, stds)], axis=0)
@@ -22,18 +23,28 @@ class Sampling():
     def __init__(self, target_dist, n_samples):
         self.target_dist = target_dist
         self.n_samples = n_samples
+        self.adapt_interval = n_samples/20
 
     def Metropolis_Hastings(self):
         """Sample from the target distribution using the Metropolis-Hastings algorithm."""
         proposal_std = 1.0
         samples = np.zeros(self.n_samples)
         x_current = 0
+        acceptance_count = 0
         for i in range(1, self.n_samples):
             x_proposal = x_current + np.random.normal(0, proposal_std)
-            acceptance_ratio = target_distribution(x_proposal) / target_distribution(x_current)
+            acceptance_ratio = target_distribution(x_proposal) / (target_distribution(x_current) + 1e-20)
             if np.random.rand() < acceptance_ratio:
                 x_current = x_proposal
             samples[i] = x_current
+            if i % self.adapt_interval == 0:
+                print(proposal_std)
+                acceptance_rate = acceptance_count / self.adapt_interval
+                if acceptance_rate < 0.2:
+                    proposal_std *= 1.1
+                elif acceptance_rate > 0.3:
+                    proposal_std *= 0.9
+                acceptance_count = 0
         return samples
 
     def plot_distrib(self, samples):
