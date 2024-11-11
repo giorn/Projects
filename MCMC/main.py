48f9c1c058a8b8b63381e8408fa1d44d6f055b8c
Gregoire Caron <g.caron1789@gmail.com>
2024-11-11 16:09:00 +0100

    Corrected some mistakes about std adaptation

--- MCMC/main.py
+++ MCMC/main.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 def target_distribution(x):
     """Return the probability at x for a mixture of Gaussian distributions."""
-    means = [4, -2]
+    means = [20, -2]
     stds = [0.5, 0.8]
     weights = [0.3, 0.7]
     proba = np.sum([w*np.exp(-0.5*((x-m)/s)**2) for w, m, s in zip(weights, means, stds)], axis=0)
@@ -28,7 +28,7 @@ class Sampling():
 
     def Metropolis_Hastings(self):
         """Sample from the target distribution using the Metropolis-Hastings algorithm."""
-        proposal_std = 1.0
+        proposal_std = 10.0
         samples = np.zeros(self.n_samples)
         x_current = 0
         acceptance_count = 0
@@ -37,21 +37,22 @@ class Sampling():
             acceptance_ratio = target_distribution(x_proposal) / (target_distribution(x_current) + 1e-20)
             if np.random.rand() < acceptance_ratio:
                 x_current = x_proposal
+                acceptance_count += 1
             samples[i] = x_current
             if i % self.adapt_interval == 0:
                 print(proposal_std)
                 acceptance_rate = acceptance_count / self.adapt_interval
                 if acceptance_rate < 0.2:
-                    proposal_std *= 1.1
-                elif acceptance_rate > 0.3:
                     proposal_std *= 0.9
+                elif acceptance_rate > 0.3:
+                    proposal_std *= 1.1
                 acceptance_count = 0
         return samples
 
     def plot_distrib(self, samples):
         """"""
-        x = np.linspace(-6, 6, 500)
-        plt.hist(samples, bins=50, density=True, alpha=0.6, color='blue', label='Samples (MCMC)')
+        x = np.linspace(min(samples) - 2, max(samples) + 2, 500)
+        plt.hist(samples, bins=100, density=True, alpha=0.6, color='blue', label='Samples (MCMC)')
         plt.plot(x, self.target_dist(x), 'r', label='Target distribution')
         plt.xlabel('x')
         plt.ylabel('Density')
@@ -59,7 +60,7 @@ class Sampling():
         plt.show()
 
 if __name__ == "__main__":
-    n_samples = 10_000
+    n_samples = 100_000
     sampling = Sampling(target_distribution, n_samples)
     samples = sampling.Metropolis_Hastings()
     sampling.plot_distrib(samples)
