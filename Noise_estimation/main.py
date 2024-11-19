d97019309104cfb8a1152d4f944d469f6b23a852
Gregoire Caron <g.caron1789@gmail.com>
2024-11-19 18:10:23 +0100

    encapsulation of noisy function into a class

--- Noise_estimation/main.py
+++ Noise_estimation/main.py
@@ -1,7 +1,7 @@
 """ 
 Author: Gregoire Caron 
 Date Created: 2024-10-31
-Last Modified: 2024-11-04
+Last Modified: 2024-11-19
 Module displaying an application of the ECNoise algorithm by More and Wild.
 """
 
@@ -9,12 +9,25 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+np.random.seed(0)
 
-def noisy_function(x, noise_std=0.01):
-    """Define a noisy function."""
-    smooth = x*x
-    noise = np.random.normal(0, noise_std, size=np.shape(x))
-    return smooth + noise
+
+def has_both_negative_and_positive(nb_list):
+    """Check if a given list of numbers contains both negative and positive numbers."""
+    has_positive = any(x > 0 for x in nb_list)
+    has_negative = any(x < 0 for x in nb_list)
+    return has_positive and has_negative
+
+class Noisy_function():
+
+    def __init__(self, noise_std=0.01):
+        self.noise_std = noise_std
+
+    def func(self, x):
+        """Define a noisy function."""
+        smooth = x*x
+        noise = np.random.normal(0, self.noise_std, size=np.shape(x))
+        return smooth + noise
 
 def plot_function(x, y):
     """Display a function (x,y)"""
@@ -23,16 +36,10 @@ def plot_function(x, y):
     plt.tight_layout()
     plt.show()
 
-def has_both_negative_and_positive(nb_list):
-    """Check if a given list of numbers contains both negative and positive numbers."""
-    has_positive = any(x > 0 for x in nb_list)
-    has_negative = any(x < 0 for x in nb_list)
-    return has_positive and has_negative
-
 def noise_estimation(f, x, h, m):
     """Compute estimate sigma_k of the noise level."""
     pts = np.arange(x-(m//2)*h, x+(m//2)*h, h)
-    delta_k = f(pts, noise_std=0.01)
+    delta_k = f(pts)
     delta_list, sigma_list = [], []
     for k in range(m):
         delta_k = np.diff(delta_k)
@@ -49,5 +56,6 @@ def noise_estimation(f, x, h, m):
             return sigma_list[l]
 
 if __name__ == "__main__":
-    noise_estimate = noise_estimation(f=noisy_function, x=1, h=1e-3, m=10)
+    Noise = Noisy_function(noise_std=0.08)
+    noise_estimate = noise_estimation(f=Noise.func, x=1, h=1e-4, m=10)
     print(noise_estimate)
